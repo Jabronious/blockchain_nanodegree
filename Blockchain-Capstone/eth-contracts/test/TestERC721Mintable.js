87fa98ff921984c53ef193ba96ea9e6856cc1d2d
Jarrod Merryman <jarrodmerryman@gmail.com>
2020-05-19 19:57:59 -0500

    completes tests

--- Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
+++ Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
@@ -8,8 +8,8 @@ contract('TestERC721Mintable', accounts => {
 
     const testingId = 436636;
 
-    describe('match erc721 spec', function () {
-        beforeEach(async function () {
+    describe('match erc721 spec', () => {
+        beforeEach(async () => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
@@ -19,27 +19,27 @@ contract('TestERC721Mintable', accounts => {
             await this.contract.mint(account_three, 123, {from: account_one});
         });
 
-        it('should return total supply', async function () { 
-            const supply = await this.contract.totalSupply();
+        it('should return total supply', async () => { 
+            const supply = await this.contract.totalSupply.call();
 
             assert.equal(supply, 4);
         });
 
-        it('should get token balance', async function () { 
-            const balance = await this.contract.balanceOf(account_two);
+        it('should get token balance', async () => { 
+            const balance = await this.contract.balanceOf.call(account_two);
 
             assert.equal(balance, 3)
         })
 
         // // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
+        it('should return token uri', async () => { 
             const actualTokenURI = 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/' + testingId;
-            const receivedTokenURI = await this.contract.tokenURI(testingId);
+            const receivedTokenURI = await this.contract.tokenURI.call(testingId);
 
             assert.equal(actualTokenURI, receivedTokenURI);
         })
 
-        it('should transfer token from one owner to another', async function () { 
+        it('should transfer token from one owner to another', async () => { 
             await this.contract.approve(account_three, testingId, {from: account_two});
             await this.contract.transferFrom(account_two, account_three, testingId, {from: account_two});
 
@@ -48,12 +48,12 @@ contract('TestERC721Mintable', accounts => {
         })
     });
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
+    describe('have ownership properties', () => {
+        beforeEach(async () => { 
             this.contract = await ERC721MintableComplete.new({from: account_one});
         })
 
-        it('should fail when minting when address is not contract owner', async function () { 
+        it('should fail when minting when address is not contract owner', async () => { 
             let error;
             try {
                 await this.contract.mint(account_two, testingId, {from: account_three});
@@ -64,7 +64,7 @@ contract('TestERC721Mintable', accounts => {
             assert.equal(error.message, 'Returned error: VM Exception while processing transaction: revert Must be the owner -- Reason given: Must be the owner.');
         })
 
-        it('should return contract owner', async function () { 
+        it('should return contract owner', async () => { 
             const contractOwner = await this.contract.owner({from: account_one});
 
             assert.equal(contractOwner, account_one);
--- Blockchain-Capstone/eth-contracts/test/TestSolnSquareVerifier.js
+++ Blockchain-Capstone/eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,33 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+const SquareVerifier = artifacts.require("SquareVerifier");
+
+contract("SolnSquareVerifier", accounts => {
+	const zokratesProof = require("../../zokrates/code/square/proof.json");
+    const proof = zokratesProof.proof;
+	const inputs = zokratesProof.inputs;
+
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+
+    before(async () => {
+        this.verifier = await SquareVerifier.new({from: account_one});
+        this.contract = await SolnSquareVerifier.new(this.verifier.address, {from: account_one});
+    });
+
+    it("should allow for a new token to be minted", async () => {
+    	const mintedToken = await this.contract.mintNewToken(
+    		account_two,
+			4325,
+			proof.a,
+        	proof.b,
+        	proof.c,
+        	inputs
+    	)
+
+        assert(mintedToken);
+    });
+});
